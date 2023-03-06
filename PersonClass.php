c4d050778d88c408cc63cdcfd2c17357e37c3f15
devhafix <124677896+devhafix@users.noreply.github.com>
2023-03-06 21:21:44 +0600

    Update PersonClass.php

--- PersonClass.php
+++ PersonClass.php
@@ -28,6 +28,23 @@ $person->setEmail('johndoe@example.com');
 echo 'Name: ' . $person->getName() . '<br>';
 echo 'Email: ' . $person->getEmail() . '<br>';
 
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  // Retrieve the user's name and email from the form
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+
+  // Create a new instance of the Person class
+  $person = new Person();
+
+  // Set the name and email properties based on the form data
+  $person->setName($name);
+  $person->setEmail($email);
+
+  // Display the name and email properties on the webpage
+  echo 'Name: ' . $person->getName() . '<br>';
+  echo 'Email: ' . $person->getEmail() . '<br>';
+}
+
 
 
 
