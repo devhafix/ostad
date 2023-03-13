59cbd01642cf28e0f7a0f61337e073de7884a9d8
devhafix <124677896+devhafix@users.noreply.github.com>
2023-03-13 16:27:54 +0600

    Create display_data.php

---
+++ module6/display_data.php
@@ -0,0 +1,31 @@
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>User Input Form</title>
+  </head>
+  <body>
+<table>
+    <thead>
+        <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Profile Picture</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+        $csv_file = fopen('users.csv', 'r');
+        while (($data = fgetcsv($csv_file)) !== false) {
+            echo '<tr>';
+            echo '<td>' . $data[0] . '</td>';
+            echo '<td>' . $data[1] . '</td>';
+            echo '<td><img src="uploads/' . $data[2] . '"></td>';
+            echo '</tr>';
+        }
+        fclose($csv_file);
+        ?>
+    </tbody>
+</table>
+     </body>
+</html>
